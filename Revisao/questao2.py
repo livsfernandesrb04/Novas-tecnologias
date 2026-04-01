0777495fa350d1d77f2b6be89ee6c9d771b716b5
Lívia Fernandes Ribeiro Bezerra <101300580+livsfernandesrb04@users.noreply.github.com>
2026-03-31 23:49:19 -0300

    Add questao2

--- Revisao/questao2.py
+++ Revisao/questao2.py
@@ -1,13 +1,27 @@
+
+transacoes = [
+    (1, "Infraestrutura", 1500.50),
+    (2, "Licenças", 450.00),
+    (3, "Infraestrutura", 3200.00),
+    (4, "Marketing", 800.00),
+    (5, "Licenças", 150.00)
+]
+
+categorias = set()
+for transacao in transacoes: 
+    categorias.add(transacao[1])
+
+print(f"Categorias: {categorias}")
+
 valorTotal = {}
 
-
-#Utilize um conjunto (set) para extrair e imprimir no console todas as categorias únicas presentes 
-# no sistema.
-#Crie um dicionário vazio. Em seguida, use um laço de repetição (for) para iterar sobre a lista de tuplas
-#  e popular esse dicionário de forma dinâmica, calculando o valor total gasto por cada categoria.
 for transacao in transacoes:
     categoria = transacao[1]
-    for categorias in valorTotal:
-        if categoria 
+    valor = transacao[2]
 
-valorTotal[categoria] = 
+    if categoria in valorTotal:
+        valorTotal[categoria] += valor
+    else:
+        valorTotal[categoria] = valor
+
+print(f"Valor por categoria: {valorTotal}")
