aeb76a28a29c7cc4ce98168da79a8dde0f5c90a7
Lívia Fernandes Ribeiro Bezerra <101300580+livsfernandesrb04@users.noreply.github.com>
2026-04-24 18:58:07 -0300

    Adicionar atv2 desafio1

--- atividade2/desafio1.py
+++ atividade2/desafio1.py
@@ -1,9 +1,8 @@
 #Criar um programa que simula o estoque de uma loja de eletrônicos.
-#Represente o estoque como uma lista de dicionários
-##cada item deve possuir nome, preco e quantidade
-
+#Representar o estoque como uma lista de dicionários
 estoque = []
 
+#cada item deve possuir nome, preco e quantidade
 def adicionar_item(nome, preco, quantidade):
 
     produto = {"nomeProduto": nome, "preço": preco, "quant": quantidade}
@@ -16,18 +15,19 @@ def calcular_total_estoque(estoque):
     for item in estoque:
         valorTotal = item["preço"] * item["quant"]
         print(f"Valor Total em estoque de {item["nomeProduto"]} : R${valorTotal}")
-        ##imprimir os itens com valor acima de R$500
+    
+    ##imprimir os itens com valor acima de R$500
+    for item in estoque:
         if item["preço"] > 500.00:
-            print(f"Nome do produto: {item["nomeProduto"]} preço: R${item["preço"]}")
+            print(f"Nome do produto: {item["nomeProduto"]}, preço: R${item["preço"]}")
         
-        
-#Gerar uma lista com os nomes dos produtos em falta(quantidade == 0)
-produtos_em_falta = [item["nomeProduto"] for item in estoque if item["quant"] == 0]
-        
-
 adicionar_item("tablet", 2765.20, 5)
 adicionar_item("mouse", 120.0, 10)
 adicionar_item("fone de ouvido", 57.90, 8)
 adicionar_item("monitor", 570.47, 0)
+adicionar_item("teclado", 230.0, 0)
 calcular_total_estoque(estoque)
-print(f"{produtos_em_falta}")
+
+#Gerar uma lista com os nomes dos produtos em falta(quantidade == 0)
+produtos_em_falta = [item["nomeProduto"] for item in estoque if (item["quant"] == 0)]
+print(f"Lista de produto em falta: {produtos_em_falta}")
--- atividade2/desafio2.py
+++ atividade2/desafio2.py
@@ -1,10 +1,27 @@
 #Criar um programa que receba uma frase do usuário e produza um relatório estístico sobre ela
+def analisar_frase(frase):
+    #Separar a frase em palavras(.split())
+    lista_palavras = frase.split()
+    #usar dicionário para contar quantas vezes cada palavra aparece
+    contagem_palavras = {}
+    
+    for palavra in lista_palavras:
+        if palavra is in contagem_palavras:
+            contagem_palavras[palavra] = contagem_palavras[palavra] + 1
+        elif palavra is not contagem_palavras:
+            contagem_palavras.update(palavra: 1)
 
-#Separar a frase em palavras(.split())
-##usar dicionário para contar quantas vezes cada palavra aparece.
+        #Identificar o número de palavras únicas na frase 
+        if contagem_palavras[palavra] == 1:
+            total_palavras_únicas =+ 1
+        
+        
+        if contagem_palavras[palavra] < 
 
-#Identificar o número de palavras únicas na frase 
-# exibir quais se repetem mais de uma vez
+    # exibir quais se repetem mais de uma vez
+
+    print(len(contagem_palavras))
+    print(total_palavras_únicas)
 
 #relatório final deve exibir:
 # total de palavras
