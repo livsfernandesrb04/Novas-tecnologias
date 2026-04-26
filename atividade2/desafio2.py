64d27704cf9736dbdd695f1fe53332a847ce4b02
Lívia Fernandes Ribeiro Bezerra <101300580+livsfernandesrb04@users.noreply.github.com>
2026-04-25 23:24:24 -0300

    Update atividade2

--- atividade2/desafio2.py
+++ atividade2/desafio2.py
@@ -2,28 +2,30 @@
 def analisar_frase(frase):
     #Separar a frase em palavras(.split())
     lista_palavras = frase.split()
-    #usar dicionário para contar quantas vezes cada palavra aparece
+
+    #Usar dicionário para contar quantas vezes cada palavra aparece
     contagem_palavras = {}
-    
     for palavra in lista_palavras:
-        if palavra is in contagem_palavras:
-            contagem_palavras[palavra] = contagem_palavras[palavra] + 1
-        elif palavra is not contagem_palavras:
-            contagem_palavras.update(palavra: 1)
-
-        #Identificar o número de palavras únicas na frase 
-        if contagem_palavras[palavra] == 1:
-            total_palavras_únicas =+ 1
+        contagem_palavras[palavra] = contagem_palavras.get(palavra, 0) + 1
         
+    palavras_repetidas = []
+    for palavra, contagem in contagem_palavras.items:
+        #Identificar o número de palavras únicas na frase
+        if contagem == 1:
+            total_palavras_únicas += 1
+        else:
+            #Adicionar as palavras que se repetem a uma lista
+            palavras_repetidas.append(palavra)
         
-        if contagem_palavras[palavra] < 
-
     # exibir quais se repetem mais de uma vez
+    lista_reorganizada = list(sorted(contagem_palavras.items(), key=lambda, item: item[1], reverse=True))
+    palavra_mais_frequente = lista_reorganizada[0]
 
-    print(len(contagem_palavras))
-    print(total_palavras_únicas)
 
-#relatório final deve exibir:
-# total de palavras
-# total de palavras únicas
-# palavra mais frequente
+    #relatório final deve exibir:
+    # total de palavras
+    print(f"Total de palavras:{len(contagem_palavras)}")
+    # total de palavras únicas
+    print(f"Total de palavras únicas: {total_palavras_únicas}")
+    # A palavra mais frequente
+    print(f"A palavra mais frequente é: {palavra_mais_frequente}")
