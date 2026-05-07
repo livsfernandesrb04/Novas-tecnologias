6b7a8139160a8e36acdfb91f1d92679e65066b67
Lívia Fernandes Ribeiro Bezerra <101300580+livsfernandesrb04@users.noreply.github.com>
2026-05-06 23:34:32 -0300

    Update desafio3.py

--- N2/Atividade2/desafio3.py
+++ N2/Atividade2/desafio3.py
@@ -2,11 +2,16 @@
 import pandas as pd
 
 #Abrir os arquivos (1) estoque_atual.csv (2) produtos.csv (3) vendas_mensal.csv 
-df1 = pd.read_csv('estoque_atual.csv')
-df2 = pd.read_csv('produto.csv')
-df3 = pd.read_csv('vendas_mensais.csv')
+estoqueAtual = pd.read_csv('estoque_atual.csv')
+produtos = pd.read_csv('produto.csv')
+vendasMensais = pd.read_csv('vendas_mensais.csv')
 
 #Fazer o merge dos 3 datasets em um só usando produto_id
+dfEstoqProd = pd.merge(estoqueAtual, produtos, on='produto_id', how='inner')
+
+print(dfEstoqProd)
+
+dfEstProdVend = pd.merge(dfEstProdVend)
 #Criar 'custo_total_estoque' = quantidade x preco_custo
 #Criar 'valor_venda_mes' = quantidade_vendida x preco_custo x 1.5(markup 50%)
 #identificar produtos com estoque zerado/negativo
